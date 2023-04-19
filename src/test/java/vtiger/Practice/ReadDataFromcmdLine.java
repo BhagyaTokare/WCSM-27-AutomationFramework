34751fe6954cca6c7d741a8c640e2b2a6403d5c1
BhagyaTokare <shreetokare02@gmail.com>
2023-04-19 12:16:27 +0530

    Chinnu commited.

--- src/test/java/vtiger/Practice/ReadDataFromcmdLine.java
+++ src/test/java/vtiger/Practice/ReadDataFromcmdLine.java
@@ -9,6 +9,10 @@ public class ReadDataFromcmdLine {
 	{
 		String BROWSER = System.getProperty("browser");//run time parameter
 		System.out.println(BROWSER);
+		
+		String BROWSER1 = System.getProperty("browser");//run time parameter
+		System.out.println(BROWSER1);
+		
 	}
 
 }
